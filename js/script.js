9ca49c493ee442df4ab8f898afa15d476d67400c
Pavel Voshchinin <pav2089@gmail.com>
2017-01-07 02:20:55 +0300

    send task only for me

--- js/script.js
+++ js/script.js
@@ -134,201 +134,103 @@ function logout() {
 	$.mobile.changePage($("#authorization"));
 }
 
-/*$(document).bind("mobileinit", function() {
-	if (navigator.platform == "iPhone" || navigator.platform == "iPad" || navigator.platform == "iPod" || navigator.platform == "iPad" || navigator.platform == "iPhone Sirnulator") {
-		//Устройство работает под управлением iOS, и мы проверим,
-		//находится ли приложение в автономном режиме
-		if (!navigator.standalone) {
-			showIOSinvitation();
-		}
-	}
-});
 
-	Мы перехватываем загрузку страницы sessions проверяем динамические данные о заседании
-	$("body").on("pageshow", "#sessions", function() {
-		if (window.localStorage!=undefined) {
-			// Интерфейс НТМL5 Local Storage доступен
-			if (window.localStorage.getItem("data") != undefined && window.localStorage.getItem("data") != null) {
-				//Вначале загружаем информацию о заседаниях, локально, хранящуюся и одновременно проверяем наличие обновлений
-				showSessions(window.localStorage.getItem("data"));
-				$("#console").html("Checking data update");
-				//Проверка наличия обновлений
-			} else {
-				//Кэш локального хранения отсутствует
-				$("#console").html("Downloading session data ... ");
-				//Загрузка информации о заседании ...
+function task_for_user() {
+	var task_for = $('input:radio[name=radio-choice-owner]:checked').val();
+	$("#input_for_email").html('');
+	if (task_for == '2') { 
+		var hop = '<legend>Email друга:</legend><input type="email" name="email_friend" id="email_friend" required>';
+		$("#input_for_email").append(hop);
+	}
+};
+
+function date_for_task() {
+	var task_for = $('input:radio[name=radio-choice-date]:checked').val();
+	$("#input_for_date").html('');
+	if (task_for == '1') { 
+		var now = new Date();
+	  	var year = now.getFullYear();
+	  	var month = now.getMonth();
+	  	var day = now.getDate();
+	  	var hour = now.getHours() + 1;
+
+	  	$('#input_for_date').append('<select name="task_deadline_year" id="task_deadline_year" data-native-menu="false"></select>');
+	  	for (i=2016;i<2026;i++){
+	  		if (i == year) {
+	  			$('#task_deadline_year').append('<option selected value="'+i+'">'+i+'</option>');
+	  		} else {
+	  			$('#task_deadline_year').append('<option value="'+i+'">'+i+'</option>');
+	  		}
+	  	}
+
+	  	$('#input_for_date').append('<select name="task_deadline_month" id="task_deadline_month" data-native-menu="false"></select>');
+	  	var arr = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+		arr.forEach(function(item, i, arr) {
+			if (i == month) {
+	  			$('#task_deadline_month').append('<option selected value="'+i+'">'+item+'</option>');
+	  		} else {
+	  			$('#task_deadline_month').append('<option value="'+i+'">'+item+'</option>');
+	  		}
+		});
+
+		$('#input_for_date').append('<select name="task_deadline_day" id="task_deadline_day" data-native-menu="false"></select>');
+	  	for (i=1;i<31;i++){
+	  		if (i == day) {
+	  			$('#task_deadline_day').append('<option selected value="'+i+'">'+i+'</option>')
+	  		} else {
+	  			$('#task_deadline_day').append('<option value="'+i+'">'+i+'</option>')
+	  		}
+	  	}
+
+	  	$('#input_for_date').append('<select name="task_deadline_hour" id="task_deadline_hour" data-native-menu="false"></select>');			
+	  	for (i=0;i<23;i++){
+	  		if (i == hour) {
+	  			$('#task_deadline_hour').append('<option selected value="'+i+'">'+i+':00</option>')
+	  		} else {
+	  			$('#task_deadline_hour').append('<option value="'+i+'">'+i+':00</option>')
+	  		}
+	  	}
+	}
+};
+
+
+function create_task_form_app() {
+	var task_name = $('#task_name').val();
+
+	var task_for = $('input:radio[name=radio-choice-owner]:checked').val();
+	var email_friend = $('#email_friend').val();
+					
+	var task_deadline_turn = $('input:radio[name=radio-choice-date]:checked').val();		
+	var task_deadline_year = $('#task_deadline_year').val();
+	var task_deadline_month = $('#task_deadline_month').val();
+	var task_deadline_day = $('#task_deadline_day').val();
+	var task_deadline_hour = $('#task_deadline_hour').val();
+
+	var key_auth = window.localStorage.getItem('key_auth');
+	$.ajax({
+		url: 'http://pinok.dh/app/import/task',
+		dataType: 'jsonp',
+		type: 'GET',
+		data: { 'task_name' : task_name, 'task_for' : task_for, 'email_friend' : email_friend, 'task_deadline_turn' : task_deadline_turn,  'task_deadline_year' : task_deadline_year, 'task_deadline_month' : task_deadline_month, 'task_deadline_day' : task_deadline_day, 'task_deadline_hour' : task_deadline_hour, 'task_type' : 62, 'key_auth' : key_auth },
+		success: function(data) {
+			if(data.status == 11){
+				alert('Задача успешно создана');
+				$('#create_task_form')[0].reset();
+				$("#input_for_email").html('');
+				$("#input_for_date").html('');
+				$.mobile.changePage($("#task_list"));
 			}
-		} else {
-			//Интерфейс НТМL5 Local Storage не поддерживается, каждый раз загружаем JSОN-файл
-			$ ("#console").html ("Downloading session data ... ");
-			//Загрузка информации о заседании ...
-		}
-		loadSessionsAjax();
-	});
-
-
-function showIOSinvitation() {
-	setTimeout(function() {
-		//Скрываем информацию о сохранении, пока не загрузится все приложение
-		$("#install").hide ();
-		//Открываем инструкцию по добавлению приложения на главный экран в iOS
-		$.mobile.changePage($("#ios"), {transition: "slideup", changeHash: false});
-	}, 100);
-	//Перехватываем события НТМL5 Application Cache для выдачи нужной информации
-	if (window.applicationCache != undefined) {
-
-		window.applicationCache.addEventListener('checking', function() {
-			$("#consoleInstall").html("Checking version");
-			//Проверка версии
-		});
-
-		window.applicationCache.addEventListener('downloading', function() {
-			$("#consoleInstall").html("Downloading application. Please wait ... ");
-			//Приложение загружается. Пожалуйста, подождите ...
-		});
-
-		window.applicationCache.addEventListener('cached', function() {
-			$("#consoleInstall").html("Application downloaded");
-			//Приложение загружено
-			$("#install").show();
-		});
-
-		window.applicationCache.addEventListener('updateready', function() {
-			$("#consoleInstall").html("Application downloaded");
-			//Приложение загружено
-			$("#install").show();
-		});
-
-		window.applicationCache.addEventListener('noupdate', function() {
-			$ ("#consoleInstall").html("Application downloaded");
-			//Приложение загружено
-			$("#install").show();
-		});
-
-		window.applicationCache.addEventListener('error', function(e) {
-			$("#consoleInstall").html("There was an error downloading this арр");
-			//Во время загрузки этого приложения произошла ошибка
-		});
-
-		window.applicationCache.addEventListener('obsolete', function(e) {
-			$("#consoleInstall") .html("There was an error downloading this арр");
-			//Во время загрузки этого приложения произошла ошибка
-		});
-	}
-}
-
-
-function loadSessionsAjax() {
-//Получаем JSОN-объект в текстовом виде для упрощения его хранения в локальной памяти
-	$.ajax("http://www.moЬilexweb.com/congress/sessions.json", {
-		complete: function(xhr) {
-			if(window.localStorage != undefined) {
-				if(window.localStorage.getItem ("data") != undefined && window.localStorage.getItem ("data") != null) {
-					if (xhr.responseText == window.localStorage.getItem("data")) {
-						//Загруженная информация о заседании совпадает с выведенной на экран
-						$("#console").html("Schedule is updated");
-						//Расписание обновлено
-					} else {
-						//Имеется новая информация о заседаниях
-						if(confirm("There is an update in the schedule available, do уои want to load it now?")) {
-							//Имеются изменеr.ия в расписании; загрузить сейчас?
-							$("#console").htrnl("Schedule is updated");
-							//Расписание обновлено
-							showSessions(xhr.responseText);
-						} else {
-							$("#console").html("Schedule will Ье updated later");
-							//Расписание будет обновлено позже
-						}
-					}
-				} else {
-					//Локальная память доступна, но кэш не обнаружен
-					$("#console").html("Schedule is updated");
-					//Расписание обновлено
-					showSessions(xhr.responseText);
-				}
-			} else {
-				//Локальная память отсутствует; показываем информацию без сохранения
-				$("#console").html("Schedule is updated");
-				//Расписание обновлено
-				showSessions(xhr.responseText);
+			if(data.status == 33){
+				alert('Авторизация устарела. Авторизуйтесь снова.');
+				$.mobile.changePage($("#authorization"));
+			}
+			if(data.status == 34){
+				alert('Ошибка в введенных данных. Попробуйте снова.');
 			}
 		},
-		dataType:'text',
-		error: function() {
-			$("#console") .htrnl("Schedule update could not Ье downloaded");
-			//Невозможно загрузить новое расписание
+		 
+		error: function(data) {
+			alert('Интернет соединение разорвано. Попробуйте позже.');
 		}
-	});
+	});	
 }
-
-
-var isFirstLoad = true;
-function showSessions(string) {
-	if (window.JSON != undefined) {
-		data = JSON.parse(string);
-	} else {
-		data = eval("(" + string + ")");
-	}
-	
-	if (window.localStorage != undefined) {
-		//Сохранить новые данные в виде строки
-		window.localStorage.setItem("data", string);
-	}
-	
-	$("#slots").html ("");
-	var html = "";
-	for (var i=O; i<data.slots.length; i++) {
-		if (data.slots[i].message!=null) {
-			//Это специальная информация, и мы создаем разделитель
-			html += "<li data-role='list-divider' data-groupingtheme='e'>" + data.slots[i].time + ": "+ data.slots[i].message + "</li>";
-		} else {
-			//Это обычная информация о заседаниях
-			html += "<li><a href='javascript:showDetails(" + i + ") '>Sessions of" + data.slots[i].time + "</a></li>";
-		}
-	}
-
-	$ ("#slots").html(html);
-	if (isFirstLoad) {
-		$("#slots").listview();
-		isFirstLoad = false;
-	} else {
-		$("#slots").listview('refresh');
-	}
-}
-
-
-function showDetails(index) {
-	$("#details h1").html("Sessions of " + data.slots[index].time);
-	var html = "";
-	for (var i=O; i<data.sessions.length; i++) {
-		if (data.sessions[i].timeId==data.slots[index].id) {
-			//Создаем по одному сворачиваемому компоненту на заседание
-			html += "<div data-role='collapsiЬle'>";
-			html += "<hЗ>" + data.sessions[i].title + "</hЗ>";
-			html += "<hЗ>" + data.sessions[i].room + "</hЗ>";
-			html += "<h4>Speaker/s: "+ data.sessions[i] .speaker;
-			html += "</h4>";
-			html += "<р>" + data.sessions[i].description + "</р>";
-			html += "</div>";
-		}
-	}
-	//Информация для страницы с подробностями
-	$("#sessionInfo").html(html);
-	$("#sessioninfo div").collapsiЬle();
-	//Переходим на страницу с подробностями
-	$.moblle.changePage($("#details"));
-}
-
-
-function refresh() {
-	//confirm("There is an update in the schedule available, do уои want to load it now?");
-	$.mobile.changePage('#task_list', "slide");
-}
-
-
-function openWithoutInstallation() {
-	//Удаляем псевдодиалоговую страницу по установке
-	$.moblle.changePage($("#home"), {transition: "slideup", reverse: true}); 
-}
-
-*/
